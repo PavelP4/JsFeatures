39cca771facbdaeadb3a8bd45f86e08200b148e0
PavelM <reginuser@gmail.com>
2017-10-07 15:45:58 +0300

    Promise, async/await

--- Promises/Main.js
+++ Promises/Main.js
@@ -1,8 +1,8 @@
 (function(){
 
-    console.log("Promises test");
+    console.log(">>>Promises test");
 
-    let a = 1;
+    let a = 10;
 
     let prm = new Promise((resolve, reject)=>{
 
@@ -10,8 +10,46 @@
 
         reject(-1);
     });
-
+    
     prm
-        .then(val => console.log("prm.resolve result is: ", val))
+        .then(val => {
+            console.log("prm.resolve result is: ", val);         
+            return 11;
+        })
+        .then(val => console.log("prm.resolve2 result is: ", val))
         .catch(err => console.log("prm.catch error is: ", err));
+
+    FuncA(15)
+        .then(val => console.log("FuncA.resolve result is: ", val));
+
+
+    
+
+    async function FuncA(val){
+
+        try{
+            await FuncB(val);
+        }
+        catch(e){
+            console.log("catch result is: ", e);    
+        }
+
+        return val;
+    }  
+    
+    async function FuncB(val){
+
+        //throw new Error("errorrrr FuncB");
+
+        return val * 10;
+    } 
+
+    Promise.all([FuncA(20), FuncB(40)])
+        .then(val => console.log("Promise.all result is: ", val))
+        .catch(err => console.log("Promise.all.catch error is: ", err));
+
+    Promise.race([FuncA(10), FuncB(100)])
+        .then(val => console.log("Promise.race result is: ", val))
+        .catch(err => console.log("Promise.race.catch error is: ", err));
+
 }())
